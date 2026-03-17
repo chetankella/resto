efcc85389a8b554838272452e71d3457023d1aaf
chetankella <chetankella117@gmail.com>
2026-03-17 09:29:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', "https://rajavaarivindhu1.netlify.app"],
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
